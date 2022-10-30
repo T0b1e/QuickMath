88997bb97f7edddd23354fe268b1027079005557
Narongkorn Kitrungrot <psuwit.narongkron.nk@gmail.com>
2022-10-30 20:07:25 +0700

    Base

---
+++ QuickMath.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	int rand_num, i;
+	rand_num = rand() / 1;
+	printf("%d\n", rand_num);
+	while True:
+		
+	for (i = 0; i <5; i++)  
+    {  
+        printf(" %d", rand() % 100);  
+    }   
+}
