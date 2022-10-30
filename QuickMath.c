39499c405e8cafd3a3097b4b5b2c1de27dc7c804
Narongkorn Kitrungrot <psuwit.narongkron.nk@gmail.com>
2022-10-30 21:45:57 +0700

    None

--- QuickMath.c
+++ QuickMath.c
@@ -11,54 +11,64 @@ int levelThree();
 int levelFour();
 int levelFive();
 
+int userInput();
+int checkAnswer();
+
+int number1, number2;
+char playerName[20];
+int playerScore = 0, playerLevel = 1, number = 1, playerAnswer;
+
 main()
 {
-	int i;
-	
-	char playerName[20];
-	int playerScore = 0, playerLevel = 1, number = 1, playerAnswer;
-	
-	srand(time(0));
-		
-	int number1 = (rand() % 99) + 1;
-	int number2 = (rand() % 99) + 1;
-	
-	// printf("%d %d", number1, number2);
-	
-	number2 = checkNumber(playerLevel,number2);
-	
 	while (1)
 	{
+		int a = 1, u = 1, c = 1;
+		
+		srand(time(0));
+
+		number1 = (rand() % 99) + 1;
+		number2 = (rand() % 99) + 1;
+		
+		number2 = checkNumber(playerLevel,number2);
+		
 		state(playerLevel, playerScore, number);
+		
 		if (playerLevel == 1)
 		{
-			levelOne(number1,number2);
-			// Check Answer
+			a = levelOne(number1,number2);
+			u = userInput();
+			c = checkAnswer(a, u, playerScore);
 		}
 		else if (playerLevel == 2)
 		{
-			levelTwo(number1,number2);
-			// Check Answer
+			a = levelTwo(number1,number2);
+			u = userInput();
+			c = checkAnswer(a, u, playerScore);
 		}
 		else if (playerLevel == 3)
 		{
-			levelThree(number1,number2);
-			// Check Answer
+			a = levelThree(number1,number2);
+			u = userInput();
+			c = checkAnswer(a, u, playerScore);
 		}
 		else if (playerLevel == 4)
 		{
-			levelFour(number1,number2);
-			// Check Answer
+			a = levelFour(number1,number2);
+			u = userInput();
+			c = checkAnswer(a, u, playerScore);
 		}		
 		else if (playerLevel == 5)
 		{
-			levelFive(number1,number2);
-			// Check Answer
+			a = levelFive(number1,number2);
+			u = userInput();
+			c = checkAnswer(a, u, playerScore);
 		}
 		else
 		{
 			printf("End Game");
 		}
+		
+		number += 1;
 		
 	}
 }
@@ -89,96 +99,58 @@ int checkNumber(int level, int number2)
 	}
 }
 
+int userInput()
+{
+	printf("\nYour Answer: ");
+    scanf("%d", &playerAnswer);
+    
+    return playerAnswer;
+}
+
+int checkAnswer(int answer, int userAnswer, int playerScore)
+{
+	if (answer == userAnswer)
+	{
+		printf("Correct\n");
+		playerScore += 10;
+	}
+	else
+	{
+		printf("Wrong\n");
+	}
+}
+
 int levelOne(int number1,int number2) //level 1
 {
 	int Ans = number1+number2;
 	printf("%d + %d = ", number1, number2);
-	printf("Your Answer: ");
-        scanf("%d", &playerAnswer);
-        printf("Your Answer: ");
-        scanf("%d", &playerAnswer);
-	if(Ans)
-	{
-		printf("Go to Next Level!:\n");
-	}
-	else
-	{
-		printf("Your Ans is Wrong!!!");
-	}
+	return Ans;
 }
 
 int levelTwo(int number1,int number2) //level 2
 {
 	int Ans = number1-number2;
 	printf("%d - %d = ", number1, number2);
-	printf("Your Answer: ");
-        scanf("%d", &playerAnswer);
-        printf("Your Answer: ");
-        scanf("%d", &playerAnswer);
-	if(Ans)
-	{
-		printf("Go to Next Level!:\n");
-	}
-	else
-	{
-		printf("Your Ans is Wrong!!!");
-	}
-	
+	return Ans;
 }
 
 int levelThree(int number1,int number2,int number3) //level 3
 {
-	int Ans = number1+number2-number3;
-	printf("%d + %d - %d = ", number1, number2,number3);
-	printf("Your Answer: ");
-        scanf("%d", &playerAnswer);
-        printf("Your Answer: ");
-        scanf("%d", &playerAnswer);
-	if(Ans)
-	{
-		printf("Go to Next Level!:\n");
-	}
-	else
-	{
-		printf("Your Ans is Wrong!!!");
-	}
-	
+	int Ans = number1+number2;
+	printf("%d + %d = ", number1, number2);
+	return Ans;
 }
 
 int levelFour(int number1,int number2) //level 4
 {
 	int Ans = number1+number2;
 	printf("%d + %d = ", number1, number2);
-	printf("Your Answer: ");
-        scanf("%d", &playerAnswer);
-        printf("Your Answer: ");
-        scanf("%d", &playerAnswer);
-	if(Ans)
-	{
-		printf("Go to Next Level!:\n");
-	}
-	else
-	{
-		printf("Your Ans is Wrong!!!");
-	}
-	
+	return Ans;
 }
 
 int levelFive(int number1,int number2) //level 5
 {
 	int Ans = number1+number2;
 	printf("%d + %d = ", number1, number2);
-	printf("Your Answer: ");
-        scanf("%d", &playerAnswer);
-        printf("Your Answer: ");
-        scanf("%d", &playerAnswer);
-	if(Ans)
-	{
-		printf("Go to Next Level!:\n");
-	}
-	else
-	{
-		printf("Your Ans is Wrong!!!");
-	}
-	
+	return Ans;
 }
